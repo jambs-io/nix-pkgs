19fa6a9d8c26cbc704580efd5c9dee2c4f755297
masavini <masavini@gmail.com>
2022-07-04 22:39:43 +0200

    Update push.sh

--- .github/workflows/push.sh
+++ .github/workflows/push.sh
@@ -1,7 +1,7 @@
 set -euxo pipefail
 
-git config user.name 'Kevin Amado'
-git config user.email kamadorueda@gmail.com
+git config user.name 'Matteo Savini'
+git config user.email masavini@gmail.com
 git pull --autostash --progress --rebase --stat origin main
 git add "${@:2}"
 git commit -m "${1}" --allow-empty
