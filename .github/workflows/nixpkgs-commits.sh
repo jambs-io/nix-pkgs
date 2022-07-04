cbb4c77271cbd6e7168957aa224255dcfc4cb92e
masavini <masavini@gmail.com>
2022-07-04 22:58:43 +0200

    Update nixpkgs-commits.sh

--- .github/workflows/nixpkgs-commits.sh
+++ .github/workflows/nixpkgs-commits.sh
@@ -1,7 +1,7 @@
 set -euxo pipefail
 
-curl -L https://nixos.org/nix/install | sh
-source ~/.nix-profile/etc/profile.d/nix.sh
+# curl -L https://nixos.org/nix/install | sh
+# source ~/.nix-profile/etc/profile.d/nix.sh
 
 git clone --branch master --single-branch https://github.com/nixos/nixpkgs src
 git -C src log --format=%H --no-merges --topo-order > tmp
