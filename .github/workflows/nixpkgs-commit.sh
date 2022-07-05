9d704bfee744ca19adce075ccbfc3d226db9c313
masavini <masavini@gmail.com>
2022-07-05 10:22:35 +0200

    process 100 commits per time

--- .github/workflows/nixpkgs-commit.sh
+++ .github/workflows/nixpkgs-commit.sh
@@ -13,7 +13,7 @@ for commit in "${commits[@]}"; do
   # Skip this commit if we already processed it
   if test -e "${target}"; then continue; fi
   # Stop after processing 25 commits
-  if test "${count}" -gt 25; then break; else count="$((count + 1))"; fi
+  if test "${count}" -gt 100; then break; else count="$((count + 1))"; fi
 
   # Query available packages in this commit or save an empty set
   src="https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz"
